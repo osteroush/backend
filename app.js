1a710f88748b5486ebdfdbeb6d25b4fb8429ded2
George Oster <georgeoster@macbook-air.myfiosgateway.com>
2023-01-28 12:58:26 -0500

    aws config for dynamo

--- app.js
+++ app.js
@@ -1,6 +1,38 @@
 const express = require('express');
 const app = express();
 
+const AWS = require('aws-sdk');
+const awsConfig = require('./awsConfig');
+
+AWS.config.update({
+    region: awsConfig.region,
+    accessKeyId: awsConfig.accessKeyId,
+    secretAccessKey: awsConfig.secretAccessKey
+});
+
+const dynamodb = new AWS.DynamoDB.DocumentClient();
+const tableName = 'Places';
+
+app.use('/api/v1/dummyPopulate', async (req, res, next) => {
+    const params = {
+        TableName: tableName,
+        Item: {
+            Name: 'Test title',
+            Date: '1/28/2023',
+            Comments: 'config file works'
+        }
+    }
+    
+    dynamodb.put(params, (err,data) => {
+        if(!err){
+            res.status(200).json(data);
+        }
+        if (err) {
+            res.status(400).json(err);
+        }
+    });
+});
+
 app.use('/api/v1/test', (req, res, next) => {
     const testResponse = {
         name: 'test',
