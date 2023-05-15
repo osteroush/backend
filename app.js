8dbf80c7efe29b6abc6fcff720361188da8c9c9e
George Oster <georgeoster@macbook-air.myfiosgateway.com>
2023-05-15 19:11:04 -0400

    app.js changes to work on a2 hosting

--- app.js
+++ app.js
@@ -26,16 +26,13 @@ const s3 = new AWS.S3({
     secretAccessKey: s3Config.secretAccessKey
 });
 
-app.post('/api/v1/place', multer().any(), async (req, res) => {
-    
+app.post('/BackEnd/api/v1/place', multer().any(), async (req, res) => {
     const images = [];
     try {
         const imageurls = await s3Utils.uploadImagesTos3(req, s3);
         images.push(...imageurls);
     } catch (error) {
-        console.log('an error happened while uploading the images:');
-        console.log(error);
-        return res.status(400).json({message: 'error encountered when uploading images.'});
+        res.status(400).json({message: 'error encountered when uploading images.', error: error});
     }
 
     try {
@@ -48,12 +45,12 @@ app.post('/api/v1/place', multer().any(), async (req, res) => {
     }    
 });
 
-app.patch('/api/v1/place', multer().any(), async (req, res) => {
+app.patch('/BackEnd/api/v1/place', multer().any(), async (req, res) => {
 
     try {
         await s3Utils.deleteImagesFroms3(req.body.imagesToDelete, s3);
     } catch (error) {
-        return res.status(400).json({message: 'error encountered when updating images.'});
+        res.status(400).json({message: 'error encountered when updating images.', error: error});
     }
 
     const images = [];
@@ -61,7 +58,7 @@ app.patch('/api/v1/place', multer().any(), async (req, res) => {
         const imageurls = await s3Utils.uploadImagesTos3(req, s3);
         images.push(...imageurls);
     } catch (error) {
-        return res.status(400).json({message: 'error encountered when uploading images.'});
+        res.status(400).json({message: 'error encountered when uploading images.', error: error});
     }
 
     try {
@@ -69,24 +66,21 @@ app.patch('/api/v1/place', multer().any(), async (req, res) => {
         const response = await dynamodb.update(params).promise();
         res.status(200).json(response);
     } catch (error) {
-        return res.status(400).json({message: 'error encountered when uploading to dynamo.'});
+        res.status(400).json({message: 'error encountered when uploading to dynamo.', error: error});
     }
 });
 
-app.get('/api/v1/places/:year', async (req, res) => {
-    
+app.get('/BackEnd/api/v1/places/:year', async (req, res) => {
     try {
         const params = dynamoUtils.constructDynamoGetPlacesParamsFrom(req, tableName);
         const dynamoResponse = await dynamodb.scan(params).promise();
         res.status(200).json(dynamoResponse);
     } catch (error) {
-        console.log('THERE WAS AN ERROR');
-        console.log(error);
-        res.status(200).json(error);
+        res.status(400).json({message: 'error encountered when getting places', error: error});  
     }
 });
 
-app.get('/api/v1/login/:user/:pass', async (req, res) => {
+app.get('/BackEnd/api/v1/login/:user/:pass', async (req, res) => {
     try {
         const params = dynamoUtils.constructDynamoLoginParamsFrom(req);
         const dynamoResponse = await dynamodb.scan(params).promise();
@@ -96,11 +90,11 @@ app.get('/api/v1/login/:user/:pass', async (req, res) => {
             res.status(200).json({login:false});
         }
     } catch (error) {
-        res.status(200).json(error);        
+        res.status(400).json({message: 'error encountered when logging in', error: error});        
     }
 });
 
-app.delete('/api/v1/place/:name/:date', async (req, res) => {
+app.delete('/BackEnd/api/v1/place/:name/:date', async (req, res) => {
     try {
         const params = dynamoUtils.constructDynamoDeleteParamsFrom(req, tableName);
         const deleteResponse = await dynamodb.delete(params).promise();
